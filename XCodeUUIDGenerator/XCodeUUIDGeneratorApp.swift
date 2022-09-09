726dfaa942f868d0447a089cf6d3821500b9dfcd
Jason Burke <burkejasonj@icloud.com>
2022-09-09 13:23:33 -0700

    Initial Commit

---
+++ XCodeUUIDGenerator/XCodeUUIDGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeUUIDGeneratorApp.swift
+//  XCodeUUIDGenerator
+//
+//  Created by Jason Burke on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct XCodeUUIDGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
